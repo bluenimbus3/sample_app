5adfa1cb4794a19a1512722ab0a882ec94b521e1
Thomas Decker <thomas_decker@t-online.de>
2011-04-10 13:49:47 +0200

    added a title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
-
+  
+  before(:each) do
+	 @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -41,6 +45,21 @@ describe PagesController do
       response.should have_selector("title",
                         :content =>
                           "Ruby on Rails Tutorial Sample App | About")
+    end  
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                        "Ruby on Rails Tutorial Sample App | Help")
     end
   end
+
 end
